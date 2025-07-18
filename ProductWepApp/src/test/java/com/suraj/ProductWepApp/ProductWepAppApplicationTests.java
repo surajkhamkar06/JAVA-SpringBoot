81ab5848d1f81c18f92723bcef6131026c445dd2
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-19 01:29:13 +0530

    ProductDataApp

---
+++ ProductWepApp/src/test/java/com/suraj/ProductWepApp/ProductWepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.ProductWepApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductWepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
