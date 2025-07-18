ee0ae3b0f75d2776602a79f3821646587064a4eb
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-19 01:19:44 +0530

    Add files via upload
    SimpleWepApp Program

---
+++ simpleWebApp/src/main/java/com/suraj/simpleWebApp/LoginController.java
@@ -0,0 +1,15 @@
+package com.suraj.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController
+{
+	@RequestMapping("/login")
+	public String login()
+	{
+		return("Login Page...!");
+	}
+
+}
