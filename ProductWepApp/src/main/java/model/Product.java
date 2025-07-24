8f40b61646bbf9266576e562ac421ab7ae2408b6
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-24 21:49:16 +0530

    Spring Boot application implementing CRUD operations with RESTful APIs

---
+++ ProductWepApp/src/main/java/model/Product.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Product 
+{
+	private int prodId;
+	private String prodName;
+	private int price;
+	
+	public Product(int prodId, String prodName, int price) {
+		super();
+		this.prodId = prodId;
+		this.prodName = prodName;
+		this.price = price;
+	}
+	
+	
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
