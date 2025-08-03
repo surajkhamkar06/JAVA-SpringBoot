57ea46ff236874a8b9fca8b4493cdc497cf36889
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-08-03 23:59:11 +0530

    Spring Security with CSRF-Token Program

---
+++ SpringSecExWithCSRF_Token/src/main/java/com/suraj/SpringSecEx/Controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.suraj.SpringSecEx.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class StudentController 
+{
+	List<Student> students = new ArrayList<>(List.of
+			       (new Student(101,"Vedant",40),
+					new Student(102,"Suraj",45)));
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request)
+	{
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents()
+	{
+		return students;	
+	}
+
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student)
+	{
+		students.add(student);
+		return student;
+	}
+}
---
+++ SpringSecExWithCSRF_Token/src/test/java/com/suraj/SpringSecEx/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.SpringSecEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
